d98cf9292655e959efdca8dc4abf173464962d3d
jakim <jakim@enliple.com>
2023-05-26 10:00:05 +0900

    [퍼블]모비온브릿지18

--- sub/site/mobon_bridge/js/mb_bridge.js
+++ sub/site/mobon_bridge/js/mb_bridge.js
@@ -1,3 +1,11 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+    console.log("resize");
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
 window.onresize = function () {
     setTimeout(function () {
         var content = document.querySelector(".navigator a.active");
