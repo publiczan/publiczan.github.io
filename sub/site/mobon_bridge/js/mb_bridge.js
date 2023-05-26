210965132d863b8c4caa14c3c9c3cdacdbacc033
jakim <jakim@enliple.com>
2023-05-26 11:18:45 +0900

    [퍼블]모비온브릿지1

--- sub/site/mobon_bridge/js/mb_bridge.js
+++ sub/site/mobon_bridge/js/mb_bridge.js
@@ -1,11 +1,18 @@
-window.onresize = function () {
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+    console.log("resize");
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+window.onresize = function () {
     setTimeout(function () {
         var content = document.querySelector(".navigator a.active");
         content.click();
     }, 500);
 }
+
 window.onload = function () {
     var header = document.querySelector('header');
     var navigator = document.querySelector('.navigator');
@@ -234,7 +241,11 @@ window.onload = function () {
                 } else {
                     header.classList.remove("first");
                 }
-
+                if (now == 7) {
+                    header.classList.add("last");
+                } else {
+                    header.classList.remove("last");
+                }
                 if (now % 2 == 1 && now < 4 || now % 2 == 1 && now < 6 || now % 2 == 1 && now < 8) {
                     navigator.classList.add("other");
                     header.classList.add("other");
