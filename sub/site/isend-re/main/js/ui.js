24e8a94ae5cc2779ed8091ff52c2d97a1546849f
jakim <jakim@enliple.com>
2025-08-19 17:01:16 +0900

    아이센드온라인폼재수정

--- sub/site/isend-re/main/js/ui.js
+++ sub/site/isend-re/main/js/ui.js
@@ -5,8 +5,38 @@ var clickev = function () {
         });
     });
     $(function () {
-        $('.top_btn').on('click', function () {
-            scrollToSection(0);
+        var combobtn = $('.cb-btn')
+
+        combobtn.on('click', function (e) {
+            e.preventDefault();
+            $(this).closest('.comboBox').toggleClass('on');
+            combobtn.not($(this)).closest('.comboBox').removeClass('on');
+        })
+        $('.cb-dt li').on('click', function (e) {
+            e.preventDefault();
+            var selectedText = $(this).text();
+            var $comboBox = $(this).closest('.comboBox');
+            var $btn = $comboBox.find('.cb-btn');
+            var $btnSpan = $btn.find('span');
+            $btnSpan.text(selectedText);
+
+            $(this).addClass('on').siblings().removeClass('on');
+            $btn.addClass('on');
+            $comboBox.removeClass('on');
+
+            if ($(this).hasClass('ot')) {
+                $comboBox.find('input.cb-input').remove();
+                $('<input type="text" class="cb-input" placeholder="직접 입력">').insertAfter($comboBox.find('.cb-btn'));
+            } else {
+                // ot 아닌 li 클릭 시 input 제거
+                $comboBox.find('input.cb-input').remove();
+            }
+        });
+
+        $(document).on('click', function (e) {
+            if (!$(e.target).closest('.comboBox').length) {
+                $('.comboBox').removeClass('on');
+            }
         });
     });
     $(function () {
@@ -64,8 +94,47 @@ var quickbtn = function () {
         } else {}
     });
 };
+var scroll = function () {
+    $(function () {
+        var $btn = $('.btn-top');
+
+        $(window).on('scroll', function () {
+            $btn.toggle($(this).scrollTop() > 100);
+        });
+
+        $btn.on('click', function () {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 500);
+        });
+    });
+}
+var animation = function () {
+    $(function () {
+        const $counters = $("section");
+        const exposurePercentage = 10;
+        const loop = true;
+        $(window).on('scroll', function () {
+            $counters.each(function () {
+                const $el = $(this);
+                const rect = $el[0].getBoundingClientRect();
+                const winHeight = window.innerHeight;
+                const contentHeight = rect.bottom - rect.top;
+                if (rect.top <= winHeight - (contentHeight * exposurePercentage / 100) && rect.bottom >= (contentHeight * exposurePercentage / 100)) {
+                    $el.addClass('on');
+                }
+                if (loop && (rect.bottom <= 0 || rect.top >= window.innerHeight)) {
+                    $el.removeClass('on');
+                }
+            });
+        }).scroll();
+    });
+}
+
 $(function () {
     clickev()
     imgev()
     quickbtn()
+    scroll()
+    animation()
 });
